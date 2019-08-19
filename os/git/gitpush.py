b984a863667210d7c5fdd53398b95a9774eda980
Hao Jiang <haojiang@kth.se>
2019-08-19 19:29:58 +0800

    auto-updated by Outlook mail

--- os/git/gitpush.py
+++ os/git/gitpush.py
@@ -4,7 +4,7 @@ sys.path.append(ROOT_DIR)
 from doc.ics.InterviewICSGenerator import InterviewICSGenerator
 
 os.chdir(ROOT_DIR)
-subprocess.call("git pull", shell=True)
+# subprocess.call("git pull", shell=True)
 InterviewICSGenerator().run()
 subprocess.call("git add .", shell=True)
 subprocess.call('git commit -m "auto-updated by Outlook mail"')
