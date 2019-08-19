1fe46dbabadbd95cdf3f094bc3599cf0ca025055
Hao Jiang <haojiang@kth.se>
2019-08-19 19:30:09 +0800

    auto-updated by Outlook mail

--- os/git/gitpush.py
+++ os/git/gitpush.py
@@ -3,7 +3,7 @@ ROOT_DIR = 'C:\\Users\\j16492\\PycharmProjects\\Scripts'
 sys.path.append(ROOT_DIR)
 from doc.ics.InterviewICSGenerator import InterviewICSGenerator
 
-os.chdir(ROOT_DIR)
+# os.chdir(ROOT_DIR)
 # subprocess.call("git pull", shell=True)
 InterviewICSGenerator().run()
 subprocess.call("git add .", shell=True)
