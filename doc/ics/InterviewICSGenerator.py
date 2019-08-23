eda94a80bd7ec5a572db20855c09f445166b704d
Hao Jiang <haojiang@kth.se>
2019-08-23 19:15:47 +0800

    auto-updated by Outlook mail

--- doc/ics/InterviewICSGenerator.py
+++ doc/ics/InterviewICSGenerator.py
@@ -2,6 +2,7 @@ import xlwings as xw
 from icalendar import Calendar, Event
 from datetime import datetime, timedelta
 import os, sys, re, json, uuid, pytz
+import traceback
 
 ROOT_DIR = 'C:\\Users\\j16492\\PycharmProjects\\Scripts'
 sys.path.append(ROOT_DIR)
@@ -85,26 +86,29 @@ class InterviewICSGenerator:
         timestamp = "not initialized"
         try:
             for interview in self.interviews:
+                #编辑时间
                 slotOriginal = interview.get(RESERVED_SLOT)
                 slotStripped = re.sub("[这本]?下*个?(星期|周|礼拜)[一二三四五六日]", "", slotOriginal)
                 slotFormated = re.sub("((10|11|12)|(0?[1-9]))[./\-月](([12][0-9])|(30|31)|(0?[1-9]))[日号]?",
                                       self._format_date, slotStripped)
-                slotReserved = re.sub("[`~!@#$%^&*()_+=|{}';,\[\].<>/?！￥…（）《》【】‘；”“’。，、？]", "", slotFormated)
+                slotReserved = re.sub("[`~!@#$%^&*()_+=|{}';,\[\].<>/?！￥…（）《》【】‘；”“’。，、？]", " ", slotFormated)
                 timestamp = TimeNormalizer().parse(target=slotReserved,
                                                    timeBase=NOW.replace(month=1, day=1, hour=0, second=0,
                                                                         microsecond=0).strftime("%Y-%m-%d %H:%M:%S"))
-                parsedTime = eval(timestamp).get("timestamp")
-
+                if eval(timestamp).get("error"):
+                    continue
+                parsedTime = eval(timestamp).get("timestamp") or eval(timestamp).get("timespan")[0]
                 dtstart = TIMEZONE.localize(datetime.strptime(parsedTime, "%Y-%m-%d %H:%M:%S")).astimezone(tz=pytz.utc)
                 dtend = dtstart + timedelta(hours=2)
-                summary = interview.get(NAME) + " " + interview.get(UNIVERSITY)
+                #编辑基本信息
                 location = interview.pop(LOCATION)
                 location = location if location else "杭州"
                 interview[LOCATION.split("\n")[0]] = location
                 interview[MOBILE] = int(interview[MOBILE])
                 description = json.dumps(interview, indent=0, sort_keys=True, ensure_ascii=False)
                 description = re.sub("[\"{},]", "", description)
-
+                summary = interview.get(NAME) + " " + interview.get(UNIVERSITY)
+                #新建事件
                 event = Event()
                 event.add("uid", "%s:%s:%s" % (dtstart.timestamp(), interview.get(MOBILE), uuid.uuid4()))
                 event.add("summary", summary)
@@ -119,16 +123,21 @@ class InterviewICSGenerator:
                 f.write(self.calendar.to_ical())
                 f.close()
         except Exception as err:
-            import traceback
-            logger.error(traceback.print_exc(), "interview: ", interview, "timestamp: ", timestamp)
+            logger.error(str(err) + str(traceback.print_exc() or " "))
+            logger.error("Interview: "+str(interview))
+            logger.error("Time: "+str(timestamp))
+
         finally:
-            xw.apps.active.quit()
+            if xw.apps.active:
+                xw.apps.active.quit()
+
 
 if __name__ == "__main__":
     try:
         InterviewICSGenerator().generate_ics()
     except Exception as err:
         import traceback
-        logger.error(traceback.print_exc())
+        logger.error(str(err) + str(traceback.print_exc() or " "))
     finally:
-        xw.apps.active.quit()
+        if xw.apps.active:
+            xw.apps.active.quit()
