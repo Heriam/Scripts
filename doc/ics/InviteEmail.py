f70503865ad8743073225c029d580ddad54d1dc6
Hao Jiang <haojiang@kth.se>
2019-10-18 20:47:43 +0800

    auto-updated by Outlook mail

--- doc/ics/InviteEmail.py
+++ doc/ics/InviteEmail.py
@@ -2,9 +2,14 @@ import os,sys
 ROOT_DIR = 'C:\\Users\\j16492\\PycharmProjects\\Scripts'
 os.chdir(ROOT_DIR+'\\doc\\ics')
 sys.path.append(ROOT_DIR)
+import logging
+logging.info("~~~")
 from emails.EmailSender import *
+logging.info("~~~")
 from jinja2 import Environment, PackageLoader
+logging.info("~~~")
 from .Constants import *
+logging.info("~~~")
 import datetime
 
 
